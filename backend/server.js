de78d589e73ce26bdb92f0e391d092ffeaa4a9e9
hokageCodes <hashsenju94@gmail.com>
2023-05-27 15:15:01 +0100

    working 27

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const authRoutes = require('./routes/auth');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://trucki-app.netlify.app/"
+}));
 
 app.use('/api/auth', authRoutes);
 
